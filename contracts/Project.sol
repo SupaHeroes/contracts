3d2466aa0343934ddc388e6811ea09d7809857f2
Richymaestro <45331793+Richymaestro@users.noreply.github.com>
2021-10-26 20:28:29 +0200

    Update Project.sol
    import ownable changed link

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
 
-import "OpenZeppelin/openzeppelin-contracts@4.3.0/contracts/access/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 //contract template for initiating a project
 contract Project is Ownable {
